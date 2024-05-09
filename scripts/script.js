ba9f8c99990bd3e278675d7d5ff834edb0ab5aaa
mrdoza <marc.mendoza@gmail.com>
2024-05-08 22:07:47 -0500

    Computer choice made created compChoice function tested and working

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log("test");
+
+
+function getComputerChoice() {
+    let choice = Math.random();
+    if (choice < 1/3) {
+        return "Rock";
+    } if (choice > 2/3) {
+        return "Scissors";
+    } else {
+        return "Paper";            
+        }   
+   }
+
+console.log(getComputerChoice());
