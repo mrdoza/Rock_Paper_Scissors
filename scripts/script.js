e65135b634d296dcb3f598c29e3d145bc647e131
mrdoza <marc.mendoza@gmail.com>
2024-05-09 21:41:02 -0500

    Major progress
    added getHumanChoice function
    added playRound function
    
    all is working as expected

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,49 @@
-
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     let choice = Math.random();
-    if (choice < 1/3) {
-        return "Rock";
-    } if (choice > 2/3) {
-        return "Scissors";
+    if (choice < 1 / 3) {
+        return "rock";
+    } if (choice > 2 / 3) {
+        return "scissors";
     } else {
-        return "Paper";            
-        }   
-   }
+        return "paper";
+    }
+}
+// console.log(getComputerChoice());
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    const choice = prompt("rock, paper or scissors", "choose one").toLowerCase();
+    if (choice == "rock" || choice == "paper" || choice == "scissors") {
+        return choice;
+    } else {
+        alert("not a choice, try again");
+        getHumanChoice();
+    }
+}
+// console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    if (humanSelection === computerSelection) {
+        alert("Tie game!");
+    }
+    else
+        if (humanSelection === "rock" && computerSelection === "scissors") {
+            alert(humanSelection + " beats " + computerSelection + "You win");
+        } else if (humanSelection === "paper" && computerSelection === "rock") {
+            alert(humanSelection + " beats " + computerSelection + "You win");
+        } else if (humanSelection === "scissors" && computerSelection === "paper") {
+            alert(humanSelection + " beats " + computerSelection + "You win");
+        } else {
+            alert(computerSelection + " beats " + humanSelection + "You lose");
+        }
+}
+
+playRound(humanSelection, computerSelection);
+
+
+
+
